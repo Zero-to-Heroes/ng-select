c6029999e7b9d7303cd812b2f8b9796045cbe8cf
sebastientromp <sebastien.tromp@gmail.com>
2018-08-09 18:35:13 +0200

    feature: trigger detect changes after state update

--- src/app/ng-select/select.component.ts
+++ src/app/ng-select/select.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, Input, OnChanges, OnInit, Output, EventEmitter, ExistingProvider, ViewChild, ViewEncapsulation, forwardRef, ElementRef, SimpleChange, SimpleChanges, ContentChild, TemplateRef} from '@angular/core';
+import {Component, HostListener, Input, OnChanges, OnInit, Output, EventEmitter, ExistingProvider, ViewChild, ViewEncapsulation, forwardRef, ElementRef, SimpleChange, SimpleChanges, ContentChild, TemplateRef, ChangeDetectorRef } from '@angular/core';
 import {NG_VALUE_ACCESSOR, ControlValueAccessor} from '@angular/forms';
 import {SelectDropdownComponent} from './select-dropdown.component';
 import {IOption} from './option.interface';
@@ -83,7 +83,8 @@ export class SelectComponent implements ControlValueAccessor, OnChanges, OnInit 
     private onTouched = () => {};
 
     constructor(
-        private hostElement: ElementRef
+        private hostElement: ElementRef,
+        private changeDetectorRef: ChangeDetectorRef
     ) {}
 
     /** Event handlers. **/
@@ -282,6 +283,7 @@ export class SelectComponent implements ControlValueAccessor, OnChanges, OnInit 
         this.placeholderView = this.optionList.hasSelected ? '' : this.placeholder;
         setTimeout(() => {
             this.updateFilterWidth();
+            this.changeDetectorRef.detectChanges();
         });
     }
 
